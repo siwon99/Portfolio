53e5d006f659436fb94404bc2c40ddc4062f4654
siwon99 <kimsiwon0707@gmail.com>
2023-03-28 21:45:31 +0900

    JS : Navbar menu click

--- main.js
+++ main.js
@@ -4,9 +4,24 @@
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
+  console.log(`window.scrollY= ${window.scrollY}` );
+  console.log(window.scrollX);
   if(window.scrollY > navbarHeight) {
     navbar.classList.add(`navbar--dark`);
   } else {
     navbar.classList.remove(`navbar--dark`);
   }
+});
+
+//버튼 클릭시 해당파트로 넘어가기
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  } 
+  console.log(event.target.dataset.link);
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({behavior: "smooth"});
 });
