3b69927bbb7403066e5e54224c3f5277689d69e6
siwon99 <kimsiwon0707@gmail.com>
2023-03-28 22:33:30 +0900

    JS : Home contact button

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ document.addEventListener('scroll', () => {
   }
 });
 
-//버튼 클릭시 해당파트로 넘어가기
+//메뉴버튼 클릭시 해당파트로 넘어가기
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
   const target = event.target;
@@ -21,7 +21,17 @@ navbarMenu.addEventListener('click', (event) => {
   if (link == null) {
     return;
   } 
-  console.log(event.target.dataset.link);
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({behavior: "smooth"});
+  scrollIntoView(link);
 });
+
+//contact 버튼 클릭시 넘어가기
+const btn = document.querySelector('.home__contact');
+btn.addEventListener('click', (event) => {
+  scrollIntoView('#contact');
+});
+
+//scrollIntoView함수를 통해 selector자리에 해당값을 받아와서 이벤트처리
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({behavior: "smooth"});
+}
